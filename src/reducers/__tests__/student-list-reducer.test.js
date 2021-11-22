6501b3fcf6f79839e42e75e6920bbc52bb89ec88
Jerax <dgjackon01@gmail.com>
2021-11-22 14:20:13 -0800

    add first passing student-reducer test

--- src/reducers/__tests__/student-list-reducer.test.js
+++ src/reducers/__tests__/student-list-reducer.test.js
@@ -2,10 +2,13 @@ import { studentReducer } from "../student-list-reducer";
 
 describe('studentReducer()', () => {
   it('should return default state', () => {
+    const action = {
+      type: 'NONE'
+    }
     const expectedState = {
       studentList: []
     }
-    const state = studentReducer();
+    const state = studentReducer(expectedState, action);
 
     expect(state).toEqual(expectedState)
   });
--- src/reducers/student-list-reducer.js
+++ src/reducers/student-list-reducer.js
@@ -5,5 +5,8 @@ const initialState = {
 }
 
 export function studentReducer(state = initialState, action) {
-
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
