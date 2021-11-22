3366d04f8efacf351605d4fe00fba399ece65ee7
Jerax <dgjackon01@gmail.com>
2021-11-22 14:29:18 -0800

    add second student-list failing reducer test

--- src/reducers/__tests__/student-list-reducer.test.js
+++ src/reducers/__tests__/student-list-reducer.test.js
@@ -1,15 +1,40 @@
 import { studentReducer } from "../student-list-reducer";
+import * as c from "../../actions/action-constants";
 
 describe('studentReducer()', () => {
   it('should return default state', () => {
     const action = {
       type: 'NONE'
-    }
+    };
+
     const expectedState = {
       studentList: []
-    }
+    };
+
     const state = studentReducer(expectedState, action);
 
-    expect(state).toEqual(expectedState)
+    expect(state).toEqual(expectedState);
+  });
+
+  it('should return student list state', () => {
+    const expectedState = [
+      {
+        name: 'foo',
+        email: 'foo@foobar.com'
+      },
+      {
+        name: 'student2',
+        email: 'student2@foobar.com'
+      }
+    ];
+
+    const action = {
+      type: c.GET_STUDENTS,
+      payload: expectedState
+    };
+
+    const state = studentReducer(action);
+
+    expect(state).toEqual(expectedState);
   });
 });
